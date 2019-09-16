1d93118ba58ef29876b80548850e957106967787
GitsMuthukumarJ <45478465+GitsMuthukumarJ@users.noreply.github.com>
2019-09-16 15:28:52 +0530

    Update Constructor.java

--- Constructor/Constructor.java
+++ Constructor/Constructor.java
@@ -1,13 +1,21 @@
 package Constructor;
+//Package name Constructor
 
 public class Constructor {
+	//Class name Constructor
 	int value1;
+	//int data type & Variable value1
 	int value2;
+	//int data type & Variable value2
 	Constructor()
+		//Created method Constructor
 	{
 	value1 =10;
+		//Value for variable
 	value2 =24;
+		//Value for variable
 	System.out.println("Inside Value is");
+		//Printing statement/syntax
 	}
 public void Display()
 {
@@ -15,8 +23,10 @@ public void Display()
 	System.out.println("Value2 ===" +value2);
 }
 public static void main (String [] args)
+	//main method
 {
 	Constructor Object = new Constructor();
+	//Object creation
 	Object.Display();
 }
 	
