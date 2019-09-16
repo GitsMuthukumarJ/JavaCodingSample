083d011c7a157e3941bb0edee12b61578cc9a2e0
GitsMuthukumarJ <45478465+GitsMuthukumarJ@users.noreply.github.com>
2019-09-16 15:43:21 +0530

    Update Employee.java

--- inheritance/Employee.java
+++ inheritance/Employee.java
@@ -1,12 +1,21 @@
 package inheritance;
+//package name inheritance
 
 class Employee{  
-	 float salary=40000;  
+	//class name Employee
+	 float salary=40000; 
+	//float datatype & variable salary & value is 40000
+	//float datatype accept decimal values
 	}  
 	class Programmer extends Employee{  
+		//Extends keyword
 	 int bonus=10000;  
+		//int datatype & variable name bonus & value is 10000
+		//int, byte, short, long these datatypes are only accepts without decimal point
 	 public static void main(String args[]){  
+		 //Main method
 	   Programmer p=new Programmer();  
+		 //Object creation
 	   System.out.println("Programmer salary is:"+p.salary);  
 	   System.out.println("Bonus of Programmer is:"+p.bonus);  
 	}  
