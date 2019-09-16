dcdb3dde5b680916024205f1aef1eb05b3ebdd23
GitsMuthukumarJ <45478465+GitsMuthukumarJ@users.noreply.github.com>
2019-09-16 15:47:57 +0530

    Update Animal.java

--- polymorphism/Animal.java
+++ polymorphism/Animal.java
@@ -1,19 +1,29 @@
 package polymorphism;
+//package name polymorphism
 
 public class Animal {
-	  public void animalSound() {
+	//Class name Animal
+	  public void animalSound() 
+	  //Method name animalSound
+	  {
 	    System.out.println("The animal makes a sound");
+		  //Printing statement/syntax
 	  }
 	}
 
 	class Pig extends Animal {
+		//extends keywords (inheritance)
 	  public void animalSound() {
+		  //Same method name with different class is polymorphism
 	    System.out.println("The pig says: wee wee");
+		  //Printing Statement/syntax
 	  }
 	}
 
 	class Dog extends Animal {
+		//Different class extends with base class
 	  public void animalSound() {
 	    System.out.println("The dog says: bow wow");
+		  //Printing statement/syntax
 	  }
 	}
